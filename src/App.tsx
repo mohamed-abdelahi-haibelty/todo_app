2bd8aedd860ba1a7295b309388c8a5c902a8708a
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-24 18:48:40 +0000

    add tab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
 import { MdDeleteOutline } from 'react-icons/md';
 import './index.css';
+import Tab from './components/Tab';
 
 
 const App = () => {
+  const tabs = ["All", "Active", "Completed"]
+  const [toggle, setToggle] = useState<number | null>(0)
+
+  const switchtab = (index:number) => {
+      setToggle(index)
+  }
+
   return (
     <div className='container w-[608px] ml-auto mr-auto px-[36px] sm:px-0'>
       <h1 className='logo text-4xl text-center text-secondry-300 pt-8 pb-14 font-bold font-body'>#Todo</h1>
+
       <div className='filter flex justify-between items-center border-b border-secondry-100 px-10 pb-[18px] mb-[18px]'>
-        <button className="all-toglle relative w-[89px] indicator text-sm font-semibold font-el indicator">All</button>
+        {/* <button className="all-toglle relative w-[89px] indicator text-sm font-semibold font-el indicator">All</button>
         <button className="active-toggle relative w-[89px]  text-sm font-semibold font-el   indicator">Active</button>
-        <button className=" completed-toggle relative w-[89px] text-sm font-semibold font-el  indicator">Completed</button>
+        <button className=" completed-toggle relative w-[89px] text-sm font-semibold font-el  indicator">Completed</button> */}
+        {tabs.map((tab, index) => <Tab label={tab} key={index} onClcik={() => switchtab(index)}  className={toggle === index ? "indicator" : ''} />)}
       </div>
+
       <div className="todo-list">
+
         <div className="add-task flex justify-between mb-8">
           <input type="text" className="px-3 outline-gray-300 font-el font-normal w-[375px] sm:w-[452px] py-5 border rounded-xl placeholder-secondry-200 text-sm" placeholder='add details' />
           <button className="py-5 px-10 rounded-xl font-el font-semibold bg-primary hover:bg-sky-700 text-sm text-white">Add</button>
         </div>
+
         <div className="tasks mb-[40px]">
+
           <div className="task flex justify-between items-center">
             <div className="flex items-center gap-[7px]">
               <input type="checkbox" className="h-6 w-6" />
@@ -25,10 +39,13 @@ const App = () => {
             </div>
             <button className="delete-task"><MdDeleteOutline className='text-secondry-100 w-6 h-6'/></button>
           </div>
+
         </div>
+
         <div className="delet-all flex justify-end">
           <button className="py-3 px-10 rounded font-el font-semibold bg-dangerRed text-xs text-white flex items-center gap-1.5"><MdDeleteOutline className='text-white w-3 h-3'/> delete all</button>
         </div>
+
       </div>
     </div>
   )
---
+++ src/components/Tab.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props{
+    className: string;
+    label: string;
+    onClcik : () => void;
+}
+
+const Tab = (props: Props) => {
+  return (
+    <button onClick={props.onClcik} className={`relative w-[89px] text-sm font-semibold font-el ${props.className}`}>{props.label}</button>
+  )
+}
+
+export default Tab
