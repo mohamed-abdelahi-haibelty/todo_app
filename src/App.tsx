0fa304980d99fa08f2c2bc6cc6c25b0a9d4b3737
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-04-24 22:24:37 +0000

    add todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,85 @@
-import {useState} from 'react'
+import {useState, ChangeEvent, useEffect} from 'react'
 import { MdDeleteOutline } from 'react-icons/md';
 import './index.css';
 import Tab from './components/Tab';
+import Task from './components/Task';
 
+export interface ITask {
+  text: string ;
+  completed: boolean;
+  id: number;
+}
 
 const App = () => {
   const tabs = ["All", "Active", "Completed"]
   const [toggle, setToggle] = useState<number | null>(0)
+  const [todos, setTodos] = useState<ITask[]>([])
+  const [inputValue, setInputvalue] = useState<string>('')
+  const [filteredTodos, setFilterdTodos] = useState<ITask[]>([])
 
   const switchtab = (index:number) => {
       setToggle(index)
   }
+
+  const getInputvalue = (e: ChangeEvent<HTMLInputElement>) => {
+      setInputvalue(e.target.value)
+  }
+
+  const CreateTodo = () => {
+    const createdtask = {text:inputValue, completed:false, id:Math.random() * 1000}
+    setTodos([...todos, createdtask])
+    setInputvalue("")
+  }
+
+  const filterHandler = (state: number | null) => {
+    switch(state){
+        case 0:
+          setFilterdTodos(todos)
+          break;
+        case 1:
+          setFilterdTodos(todos.filter((todo:ITask) => !todo.completed))
+          break;
+        case 2:
+          setFilterdTodos(todos.filter((todo:ITask) => todo.completed))
+          break;
+          
+    }
+  }
+  
+  const removeAllTodo = () => {
+        setTodos([])
+  }
+
+  useEffect(() => {
+    filterHandler(toggle)
+  }, [toggle, todos])
 
   return (
     <div className='container w-[608px] ml-auto mr-auto px-[36px] sm:px-0'>
       <h1 className='logo text-4xl text-center text-secondry-300 pt-8 pb-14 font-bold font-body'>#Todo</h1>
 
       <div className='filter flex justify-between items-center border-b border-secondry-100 px-10 pb-[18px] mb-[18px]'>
-        {/* <button className="all-toglle relative w-[89px] indicator text-sm font-semibold font-el indicator">All</button>
-        <button className="active-toggle relative w-[89px]  text-sm font-semibold font-el   indicator">Active</button>
-        <button className=" completed-toggle relative w-[89px] text-sm font-semibold font-el  indicator">Completed</button> */}
+
         {tabs.map((tab, index) => <Tab label={tab} key={index} onClcik={() => switchtab(index)}  className={toggle === index ? "indicator" : ''} />)}
+
       </div>
 
       <div className="todo-list">
 
         <div className="add-task flex justify-between mb-8">
-          <input type="text" className="px-3 outline-gray-300 font-el font-normal w-[375px] sm:w-[452px] py-5 border rounded-xl placeholder-secondry-200 text-sm" placeholder='add details' />
-          <button className="py-5 px-10 rounded-xl font-el font-semibold bg-primary hover:bg-sky-700 text-sm text-white">Add</button>
+          <input type="text" value={inputValue} onChange={(e) => getInputvalue(e)} className="px-3 outline-gray-300 font-el font-normal w-[375px] sm:w-[452px] py-5 border rounded-xl placeholder-secondry-200 text-sm" placeholder='add details' />
+          <button onClick={CreateTodo} className="py-5 px-10 rounded-xl font-el font-semibold bg-primary hover:bg-sky-700 text-sm text-white">Add</button>
         </div>
 
         <div className="tasks mb-[40px]">
 
-          <div className="task flex justify-between items-center">
-            <div className="flex items-center gap-[7px]">
-              <input type="checkbox" className="h-6 w-6" />
-              <p className="task-content text-black font-medium text-[18px] font-el leading-[22px]">Do coding challenges</p>
-            </div>
-            <button className="delete-task"><MdDeleteOutline className='text-secondry-100 w-6 h-6'/></button>
-          </div>
+          {filteredTodos.map((todo: ITask, index:number) => <Task currentTodo={todo} key={index} todos={todos} setTodos={setTodos}/>)}
 
         </div>
 
-        <div className="delet-all flex justify-end">
-          <button className="py-3 px-10 rounded font-el font-semibold bg-dangerRed text-xs text-white flex items-center gap-1.5"><MdDeleteOutline className='text-white w-3 h-3'/> delete all</button>
-        </div>
+        {toggle === 2 && (<div className="delet-all flex justify-end transition ease-out duration-900">
+          <button onClick={removeAllTodo} className="py-3 px-10 rounded font-el font-semibold bg-dangerRed text-xs text-white flex items-center gap-1.5 "><MdDeleteOutline className='text-white w-3 h-3'/> delete all</button>
+        </div>)}
 
       </div>
     </div>
---
+++ src/components/Task.tsx
@@ -0,0 +1,46 @@
+import {ChangeEvent} from 'react'
+import { MdDeleteOutline } from 'react-icons/md';
+import { ITask } from '../App';
+
+interface Props{
+    todos: ITask[];
+    currentTodo: ITask;
+    setTodos: React.Dispatch<React.SetStateAction<ITask[]>>
+}
+
+const Task = (props: Props) => {
+  const deleteTask = () => {
+    props.setTodos(props.todos.filter((todo) => todo.id !== props.currentTodo.id))
+  }
+
+  const checkTask = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.checked){
+        props.setTodos(props.todos.map((todo: ITask) => {
+            if(todo.id === props.currentTodo.id){
+                return {...todo, completed:true}
+            }
+            return todo
+        }))
+    }else{
+        props.setTodos(props.todos.map((todo: ITask) => {
+            if(todo.id === props.currentTodo.id){
+                return {...todo, completed:false}
+            }
+            return todo
+        }))
+    }
+
+  }
+
+  return (
+    <div className="task flex justify-between items-center mb-6">
+        <div className="flex items-center gap-[7px]">
+        <input type="checkbox" checked={props.currentTodo.completed} onChange={(e) => checkTask(e)} className="h-6 w-6" />
+        <p className={`task-content text-black font-medium text-[18px] font-el leading-[22px] ${props.currentTodo.completed? "line-through": ""}`}>{props.currentTodo.text}</p>
+        </div>
+        <button className="delete-task" onClick={deleteTask}><MdDeleteOutline className='text-secondry-100 w-6 h-6'/></button>
+    </div>
+  )
+}
+
+export default Task
